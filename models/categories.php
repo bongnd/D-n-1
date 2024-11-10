9d4aad74c10683865b8b970e1781be5fc3b0c063
bongnd <bong19062005@gmail.com>
2024-11-10 17:57:30 +0700

    Mô tả thay đổi

--- models/categories.php
+++ models/categories.php
@@ -3,16 +3,16 @@ include_once 'commos/function.php';
 
 function get_categories(){
 
-    $sql="SELECT * FROM categories";
+    $sql="SELECT * FROM danhmuc";
     $conn= pdo_get_connection();
     $stmt =$conn->prepare($sql);
     $stmt->execute();
     return $stmt->fetchAll();
 }
 function get_categoriesID($id){
-    return pdo_query_one("SELECT * FROM categories WHERE categories_id= ?",$id);
+    return pdo_query_one("SELECT * FROM danhmuc WHERE danhmuc= ?",$id);
 
 }
 function get_danhmuc(){
-    return pdo_query("SELECT *FROM categories");
+    return pdo_query("SELECT *FROM danhmuc");
 }
